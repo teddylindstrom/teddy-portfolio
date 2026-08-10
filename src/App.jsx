1e4573472410b8269c9b5541d019dba4fb5952d3
teddylindstrom <teddy.lindstrom.work@gmail.com>
2026-08-10 19:06:58 +0200

    navbar boiler plate

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Experience from "./components/Experience";
-import Education from "./components/Education";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Experience />
-      <Education />
-      <Contact />
-      <Footer />
     </>
   );
 }
