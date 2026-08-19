d15b98678ce9d5e5947684859899c65d8c207753
teddylindstrom <teddy.lindstrom.work@gmail.com>
2026-08-19 14:18:15 +0200

    contact info

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Experience />
       <Projects />
+      <Contact />
     </>
   );
 }
